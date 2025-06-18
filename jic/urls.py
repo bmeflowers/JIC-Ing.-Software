08eb45f03bc8fcc85e2a88ac656df6a7e1cff73a
Tucker <thomasitothebest@gmail.com>
2025-06-17 21:40:04 -0500

    Vacantes

--- jic/urls.py
+++ jic/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('users/', include('users.urls')),
+    path('dashboard/', include('dashboards.urls')),
+    path('vacantes/', include('vacantes.urls')),
 ]
--- vacantes/models.py
+++ vacantes/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from users.models import EmpresaProfile
 
 # Create your models here.
+class Vacante(models.Model):
+    empresa = models.ForeignKey(EmpresaProfile, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=255)
+    descripcion = models.TextField()
+    requerimientos = models.TextField()
+    ubicacion = models.CharField(max_length=100)
+    rango_salario = models.CharField(max_length=100, blank=True, null=True)
+    fecha_publicada = models.DateTimeField(auto_now_add=True)
+    fecha_limite = models.DateField()
+    activo = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "Vacante"
+        verbose_name_plural = "Vacantes"
+        ordering = ['-fecha_publicada']
+
+    def __str__(self):
+        return self.empresa.nombre_empresa
