1834defc40f048b1843918d6b011775b50868ac5
Tucker <thomasitothebest@gmail.com>
2025-06-19 15:08:19 -0500

    vista de postulaciones

--- postulaciones/urls.py
+++ postulaciones/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
 
 urlpatterns = [
+<<<<<<< Updated upstream
     
+=======
+    path('postular/<int:vacante_id>/', views.postular_vacante, name='postular_vacante'),
+    path('mis-postulaciones/', views.ver_postulaciones, name='ver_postulaciones'),
+    
+    path('empresa/mis-vacantes-postulaciones/', views.PostulacionesVacantesEmpresaListView.as_view(), name='postulaciones_vacantes_empresa_lista'),
+    path('empresa/vacante/<int:pk>/postulaciones/', views.PostulacionesEmpresaListView.as_view(), name='postulaciones_empresa_lista'),
+    path('empresa/postulacion/<int:pk>/detalles/', views.PostulacionEmpresaDetailView.as_view(), name='postulacion_empresa_detalles')
+>>>>>>> Stashed changes
 ]
